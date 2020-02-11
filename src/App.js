0282862d6c1bdbf109f76a1d3c344f714f57807e
Jaye Anne Laguardia <jayelaguardia@gmail.com>
2020-02-11 01:54:58 -0800

    saving

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import config from './config';
+
+function checkConnection(){
+  fetch(config.API_ENDPOINT)
+  .then(response => response.json())
+  .then(responseJson => {
+    console.log(responseJson.message)
+  })
+  .catch(error => console.log(error))
+}
 
 function App() {
+
   return (
     <main className='App'>
-      {/* content goes here */}
+      <h1>Bobapedia</h1>
+      {checkConnection()}
     </main>
   );
 }
